6e6c1c401a35965db961e364cd15cb93b3d280ff
Cynthia Njomo <cynthiangoirinjomo@gmail.com>
2022-08-14 01:51:34 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+function saturdayFun(activity="roller-skate"){
+    return `This Saturday, I want to ${activity}!` 
+  
+    
+}
+saturdayFun ("bathe the dog");
+
+const mondayWork = function(activity="go to the office") {
+
+    return `This Monday, I will ${activity}.`
+}
+mondayWork("work from home");
+    
+
+function wrapAdjective(result="*"){
+    return function (emphatic="a hard worker"){
+        return `You are ${result}${emphatic}${result}!`
+
+    };
+}
+wrapAdjective( "||") ("a dedicated programmer");
